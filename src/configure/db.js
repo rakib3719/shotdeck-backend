a5a459dbc341c1e38a258e7f5399e54b9015f188
rakib3719 <bannah76769@gmail.com>
2025-06-21 09:48:48 +0600

    mongodb database intregation

--- src/configure/db.js
+++ src/configure/db.js
@@ -1,21 +1,51 @@
 
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
+// dotenv.config();
+
+// const connectDB = async()=>{
+
+
+// try {
+//     const connect = await mongoose.connect('mongodb+srv://shotdeck2:ROS0KZoq5HDmOlbz@cluster0.5sfxlm2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+//         dbName: 'shotdeck', 
+//       });
+//    console.log(`moongdo db connected `);
+    
+// } catch (error) {
+//     console.log(error.message, "error");
+// }
+// }
+
+// export default connectDB;
+
+
+
+
+
+
+
+
+
+
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
 dotenv.config();
 
-const connectDB = async()=>{
-
-
-try {
-    const connect = await mongoose.connect('mongodb+srv://shotdeck2:ROS0KZoq5HDmOlbz@cluster0.5sfxlm2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-        dbName: 'shotdeck', 
-      });
-   console.log(`moongdo db connected `);
-    
-} catch (error) {
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(
+      "mongodb://127.0.0.1:27017",
+      {
+        dbName: 'shotdeck',
+      }
+    );
+    console.log(`mongodb db connected`);
+  } catch (error) {
     console.log(error.message, "error");
-}
+  }
 }
 
 export default connectDB;
