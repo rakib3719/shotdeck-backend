35a19cdd10e614756b56dd9eb405c752b1a5e4dd
rakib3719 <bannah76769@gmail.com>
2025-06-21 11:17:33 +0600

    update ssl

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ app.use(cors({
     "http://fx-references.com:3000",
     "http://fx-references.com",
     "https://fx-references.com:3000",
-    "https://fx-references.com",
-    "http://31.97.156.58"
+    "http://31.97.156.58",
+    "https://fx-references.com"
 
     
     
